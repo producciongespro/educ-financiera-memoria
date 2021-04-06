a7d274c77707bce8b3e179b8fdd2f3650756656a
PatriciaHdz <32460268+patyhc4oct@users.noreply.github.com>
2021-04-06 10:59:56 -0600

    Genera carpeta del Built
    Para trasladar al servidor de pruebas y enviar a revisión

---
+++ Build/Memoria/offline.js
@@ -0,0 +1,28 @@
+﻿{
+	"version": 1617726913,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/tarjeta-sheet0.png",
+		"images/tarjeta-sheet1.png",
+		"images/fondo-sheet0.png",
+		"images/spinner-sheet0.png",
+		"images/btninicio-sheet0.png",
+		"images/btnacercade-sheet0.png",
+		"images/ventanaresumen-sheet0.png",
+		"images/btncerrar-sheet0.png",
+		"media/bambu_1.ogg",
+		"media/correcto.ogg",
+		"media/error.ogg",
+		"media/ganador.ogg",
+		"media/incorrecta.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
